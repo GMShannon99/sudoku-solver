2e5febb72d67ffe73ff262bba52430a45d6558f1
Gil Shannon <gmshannon99@gmail.com>
2026-08-28 21:15:04 -0400

    Swap in harder puzzle to expose naked-singles limitation

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,13 +1,16 @@
+# A famous "world's hardest sudoku"-style puzzle (only 21 givens).
+# Naked singles alone will NOT fully solve this one -- it's meant to stall
+# out, which is exactly what we need to build and test backtracking against.
 sample_puzzle = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+    [0, 9, 0, 0, 0, 0, 4, 0, 0],
 ]
  
 ALL_DIGITS = set(range(1, 10))
